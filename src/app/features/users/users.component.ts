c27225eb4518896a83f78e1818831da08e7f388d
Bittu kumar <bittu.kumar@thermofisher.com>
2025-12-16 07:58:58 +0530

    signal example

--- src/app/features/users/users.component.ts
+++ src/app/features/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, OnInit } from '@angular/core';
+import { Component, inject, signal, OnInit, computed, effect } from '@angular/core';
 import { DatePipe, CommonModule } from '@angular/common';
 import { AuthService } from '../../core/services/auth.service';
 import { NavigationComponent } from '../../shared/components/navigation/navigation.component';
@@ -17,7 +17,53 @@ export class UsersComponent implements OnInit {
   readonly isLoading = signal(false);
   readonly error = signal<string | null>(null);
 
+// I am using this space to learn signal
+
+// Writable signal
+  celsius = signal(0);
+
+  // Derived value (auto-calculated)
+  fahrenheit = computed(() => (this.celsius() * 9/5) + 32);
+
+  constructor() {
+    // Effect — runs whenever celsius changes
+    effect(() => {
+      console.log("Celsius changed:", this.celsius());
+    });
+  }
+
+  // Using update() when value depends on the previous value
+  increase() {
+    this.celsius.update(v => v + 1);
+  }
+
+  decrease() {
+    this.celsius.update(v => v - 1);
+  }
+
+  increaseBy(value: number) {
+    this.celsius.update(v => v + value);
+  }
+
+  decreaseBy(value: number) {
+    this.celsius.update(v => v - value);
+  }
+
+  // Using set() when replacing directly
+  reset() {
+    this.celsius.set(0);
+  }
+
+  // For input field
+  updateTemp(value: string) {
+    this.celsius.set(Number(value));
+  }
+
   ngOnInit(): void {
+
+    
+
+
     this.loadUsers();
   }
 
